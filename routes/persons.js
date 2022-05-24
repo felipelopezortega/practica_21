3e1f63619c964a6013a8b65289173e0026c29292
visita <visita@TELEMATICA.UCOL.MX>
2022-05-24 07:50:21 -0500

    The links between the pages are now done and working successfully

--- routes/persons.js
+++ routes/persons.js
@@ -17,6 +17,24 @@ router.get("/person", function(req,res){
     res.render("person");
 });
 
+//tercera ruta, es la página de inicio 
+router.get("/", function(req,res){
+
+    res.render("index");
+});
+
+//cuarta ruta, hacia el listado de los datos de la base de datos
+router.get("/listado", function(req,res){
+
+    res.render("listado");
+});
+
+//ruta auxiliar para la página de inicio 
+router.get("/inicio", function(req,res){
+
+    res.render("index");
+});
+
 //primera ruta post, para guardar los datos llenados en el formulario
 router.post("/personssend",function (req, res) {
 
